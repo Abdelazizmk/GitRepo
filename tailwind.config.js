619edddbfa1ea7d27ae2792962ac75334e94c27a
Abdelaziz Mostafa <Abdulazizmk1999@gmail.com>
2023-06-14 15:31:42 +0400

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Proxima Nova', 'ui-sans-serif', 'system-ui'],
+        'averta': ['Averta Demo PE Cutted Demo', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
+
